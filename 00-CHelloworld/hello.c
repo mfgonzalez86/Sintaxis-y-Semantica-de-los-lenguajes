615e7373cff44ea69a8ed8ea52f0b46c9396009d
mfgonzalez86 <37709442+mfgonzalez86@users.noreply.github.com>
2021-04-09 23:16:26 -0300

    creacion hello.c

---
+++ 00-CHelloworld/hello.c
@@ -0,0 +1,9 @@
+/* 00-Hello
+ *Demostrar que se esta en capacidad de etiar,compilar y ejecutar un programa en C
+ *Gonzalez Sintas Maria Fernanda
+ *09042021
+*/
+#include <stdio.h>
+int main(void)  {
+    printf ("Hello, World!\n");
+}
