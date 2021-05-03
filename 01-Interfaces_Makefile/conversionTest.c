48e1fff3d928d59b959611687990434b0cca6f86
mfgonzalez86 <37709442+mfgonzalez86@users.noreply.github.com>
2021-05-02 23:01:24 -0300

    Add files via upload
    Test

---
+++ 01-Interfaces_Makefile/conversionTest.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <assert.h>
+#include<math.h>
+#include "Conversion.h"
+
+
+
+int main(void){
+/*Funcion Celsius:Convierte de Farenheit a Celsius*/
+assert (138.00==round(Celsius(280)));
+assert (38.00==round(Celsius(100)));
+/*Funcion Farenheit: Convierte de Celsius a Farenheit*/
+assert (Farenheit(0)==32.00);
+assert (Farenheit(60)==140.00);
+}
